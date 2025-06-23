d0f17926186316a8e1027b1c5e06469cf1ae82c7
hemantk77 <hemant11uk@gmail.com>
2025-06-23 23:42:50 +0200

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -1,6 +1,7 @@
 <?php
-$conn = new mysqli('localhost', 'root', '', 'stock_app_db');
+$conn = new mysqli("localhost", "root", "password123", "stock_app_db", 3307);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
