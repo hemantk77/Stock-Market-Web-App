ea6d1e189a3a88a0d1f13c8d4c8d37c19ba1c244
hemantk77 <hemant11uk@gmail.com>
2025-07-02 15:41:52 +0200

    Update portfolio.php

--- portfolio.php
+++ portfolio.php
@@ -135,11 +135,22 @@ if (isset($stockDataApiResponse['data'])) {
 
     <nav class="navbar fixed-bottom bottom-nav">
         <div class="container-fluid d-flex justify-content-around">
-            <a href="portfolio.php" class="nav-link active text-center"><i class="bi bi-wallet-fill"></i><div class="small">Portfolio</div></a>
-            <a href="dashboard.php" class="nav-link text-center"><i class="bi bi-newspaper"></i><div class="small">Overview</div></a>
-            <a href="markets.php" class="nav-link text-center"><i class="bi bi-graph-up"></i><div class="small">Markets</div></a>
-            <a href="#" class="nav-link text-center"><i class="bi bi-search"></i><div class="small">Search</div></a>
-            <a href="profile.php" class="nav-link text-center"><i class="bi bi-person-circle"></i><div class="small">Profile</div></a>
+            <a href="portfolio.php" class="nav-link text-center">
+            <i class="bi bi-wallet-fill"></i>
+            <div class="small">Portfolio</div>
+            </a>
+            <a href="dashboard.php" class="nav-link text-center">
+            <i class="bi bi-newspaper"></i>
+            <div class="small">Overview</div>
+            </a>
+            <a href="markets.php" class="nav-link text-center">
+            <i class="bi bi-graph-up"></i>
+            <div class="small">Markets</div>
+            </a>
+            <a href="profile.php" class="nav-link text-center">
+            <i class="bi bi-person-circle"></i>
+            <div class="small">Profile</div>
+            </a>
         </div>
     </nav>
     <div style="padding-bottom: 80px;"></div>
